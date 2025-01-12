1311ac9f744c13b489330081ddab24fa5d6bca0a
x404 MASTER <71309968+MasterHide@users.noreply.github.com>
2025-01-13 03:37:24 +0530

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -5,21 +5,37 @@
 # Author:   MasterHide
 
 echo -n "Blocking public trackers ... "
-wget -q -O/etc/trackers https://raw.githubusercontent.com/MasterHide/block-publictorrent-iptables/main/trackers
+wget -q -O /etc/trackers https://raw.githubusercontent.com/MasterHide/block-publictorrent-iptables/main/trackers
+if [ $? -ne 0 ]; then
+    echo "Failed to download trackers file."
+    exit 1
+fi
+
 cat >/etc/cron.daily/denypublic<<'EOF'
 IFS=$'\n'
+IPTABLES_CMD=$(which iptables)
 L=$(/usr/bin/sort /etc/trackers | /usr/bin/uniq)
 for fn in $L; do
-        /sbin/iptables -D INPUT -d $fn -j DROP
-        /sbin/iptables -D FORWARD -d $fn -j DROP
-        /sbin/iptables -D OUTPUT -d $fn -j DROP
-        /sbin/iptables -A INPUT -d $fn -j DROP
-        /sbin/iptables -A FORWARD -d $fn -j DROP
-        /sbin/iptables -A OUTPUT -d $fn -j DROP
+        $IPTABLES_CMD -D INPUT -d $fn -j DROP
+        $IPTABLES_CMD -D FORWARD -d $fn -j DROP
+        $IPTABLES_CMD -D OUTPUT -d $fn -j DROP
+        $IPTABLES_CMD -A INPUT -d $fn -j DROP
+        $IPTABLES_CMD -A FORWARD -d $fn -j DROP
+        $IPTABLES_CMD -A OUTPUT -d $fn -j DROP
 done
 EOF
 chmod +x /etc/cron.daily/denypublic
+
 curl -s -LO https://raw.githubusercontent.com/MasterHide/block-publictorrent-iptables/main/hostsTrackers
-cat hostsTrackers >> /etc/hosts
-sort -uf /etc/hosts > /etc/hosts.uniq && mv /etc/hosts{.uniq,}
-echo "${OK}"
+if [ $? -ne 0 ]; then
+    echo "Failed to download hostsTrackers."
+    exit 1
+fi
+
+cat hostsTrackers | sort -uf >> /etc/hosts
+if [ $? -ne 0 ]; then
+    echo "Failed to update /etc/hosts."
+    exit 1
+fi
+
+echo "Blocking public trackers completed successfully."
